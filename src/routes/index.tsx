5aafd9eb9d84f512e618ddb46a9a8597a74b17cf
tpxxy1311 <65901921+tpxxy1311@users.noreply.github.com>
2024-01-03 15:38:18 +0100

    change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ export default component$(() => {
       <div class="container container-center container-spacing-xl">
         <h3>
           You can <span class="highlight">count</span>
-          <br /> on me
+          <br /> on me!
         </h3>
         <Counter />
       </div>
